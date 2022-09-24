daee84e0a007bb5c43e93b5ec4912dadb833ea67
a1234 <nwang@nfinitymint.com>
2022-09-24 23:39:14 +0800

    update HelloController and workflow for making the command run in the backgroud

--- src/main/java/com/nfinity/controller/HelloController.java
+++ src/main/java/com/nfinity/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String test(){
-        return "hello, nft123";
+        return "hello, Cat";
     }
 }
