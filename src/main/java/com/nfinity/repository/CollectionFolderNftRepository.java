a5d25ea50155d9241be4fc7b040787ba169c8bd6
a1234 <nwang@nfinitymint.com>
2022-10-13 16:57:42 +0800

    1. remove table draft_collection and draft_collection_folder_nft

--- src/main/java/com/nfinity/repository/CollectionFolderNftRepository.java
+++ src/main/java/com/nfinity/repository/CollectionFolderNftRepository.java
@@ -4,6 +4,9 @@ import com.nfinity.entity.CollectionFolderNftEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CollectionFolderNftRepository extends JpaRepository<CollectionFolderNftEntity, Long> {
+    List<CollectionFolderNftEntity> findAllByCollectionId(Long collectionId);
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,22 +3,22 @@ use nft_business;
 
 create table IF NOT EXISTS collection(
     id bigint PRIMARY KEY AUTO_INCREMENT,
-    name varchar(64) NOT NULL,
-    icon varchar(1024) NOT NULL,
-    category int not null,
-    domain_name varchar(128) not null,
-    contract_chain varchar(128) NOT NULL,
-    symbol varchar(32) not null,
-    total_supply int not null,
-    mint_price decimal not null,
-    description varchar(1024) not null,
-    airdrop_retention int not null,
+    name varchar(64),
+    icon varchar(1024),
+    category int,
+    domain_name varchar(128),
+    contract_chain varchar(128),
+    symbol varchar(32),
+    total_supply int,
+    mint_price decimal,
+    description varchar(1024),
+    airdrop_retention int,
     retained_qty int,
-    revenue decimal NOT NULL,
-    address varchar(128) NOT NULL,
-    minted_qty int NOT NULL,
+    revenue decimal,
+    address varchar(128),
+    minted_qty int,
     status int NOT NULL, -- 1-drafted, 2-pending, 3-published, 4-suspended
-    contract_status int not null, -- 1-init, 2-pending, 3-published, 4-failed
+    contract_status int, -- 1-init, 2-pending, 3-published, 4-failed
     create_time timestamp not null,
     update_time timestamp not null
 );
@@ -60,40 +60,40 @@ create table if not exists collection_folder_nft(
     create_time timestamp not null,
     update_time timestamp not null
 );
-create unique index index_collection_folder_nft_nft_id on collection_folder_nft (nft_id);
+create index index_collection_folder_nft_nft_id on collection_folder_nft (nft_id);
 create index index_collection_folder_nft_folder_id on collection_folder_nft (folder_id);
 create index index_collection_folder_nft_collection_id on collection_folder_nft (collection_id);
 
-create table IF NOT EXISTS draft_collection(
-    id bigint PRIMARY KEY AUTO_INCREMENT,
-    name varchar(64),
-    icon varchar(1024),
-    category int not null,
-    domain_name varchar(128),
-    contract_chain varchar(128),
-    symbol varchar(32) ,
-    total_supply int,
-    mint_price decimal,
-    description varchar(1024),
-    airdrop_retention int,
-    retained_qty int,
-    status int NOT NULL, -- 1-drafted
-    create_time timestamp not null,
-    update_time timestamp not null
-);
-
-create table if not exists draft_collection_folder_nft(
-    id bigint primary key auto_increment,
-    collection_id bigint,
-    folder_id bigint,
-    nft_id bigint,
-    nft_status int, -- 1-disable, 2-enable
-    create_time timestamp not null,
-    update_time timestamp not null
-);
-create unique index index_draft_collection_folder_nft_nft_id on draft_collection_folder_nft (nft_id);
-create index index_draft_collection_folder_nft_folder_id on draft_collection_folder_nft (folder_id);
-create index index_draft_collection_folder_nft_collection_id on draft_collection_folder_nft (collection_id);
+# create table IF NOT EXISTS draft_collection(
+#     id bigint PRIMARY KEY AUTO_INCREMENT,
+#     name varchar(64),
+#     icon varchar(1024),
+#     category int not null,
+#     domain_name varchar(128),
+#     contract_chain varchar(128),
+#     symbol varchar(32) ,
+#     total_supply int,
+#     mint_price decimal,
+#     description varchar(1024),
+#     airdrop_retention int,
+#     retained_qty int,
+#     status int NOT NULL, -- 1-drafted
+#     create_time timestamp not null,
+#     update_time timestamp not null
+# );
+#
+# create table if not exists draft_collection_folder_nft(
+#     id bigint primary key auto_increment,
+#     collection_id bigint,
+#     folder_id bigint,
+#     nft_id bigint,
+#     nft_status int, -- 1-disable, 2-enable
+#     create_time timestamp not null,
+#     update_time timestamp not null
+# );
+# create unique index index_draft_collection_folder_nft_nft_id on draft_collection_folder_nft (nft_id);
+# create index index_draft_collection_folder_nft_folder_id on draft_collection_folder_nft (folder_id);
+# create index index_draft_collection_folder_nft_collection_id on draft_collection_folder_nft (collection_id);
 
 create table if not exists user
 (
@@ -107,3 +107,37 @@ create table if not exists user
 );
 create unique index index_user_email on user (email);
 create unique index index_user_user_name on user (user_name);
+
+alter table collection
+    modify name varchar(64) null;
+alter table collection
+    modify icon varchar(1024) null;
+alter table collection
+    modify category int null;
+alter table collection
+    modify domain_name varchar(128) null;
+alter table collection
+    modify contract_chain varchar(128) null;
+alter table collection
+    modify symbol varchar(32) null;
+alter table collection
+    modify total_supply int null;
+alter table collection
+    modify mint_price decimal null;
+alter table collection
+    modify description varchar(1024) null;
+alter table collection
+    modify airdrop_retention int null;
+alter table collection
+    modify revenue decimal null;
+alter table collection
+    modify address varchar(128) null;
+alter table collection
+    modify minted_qty int null;
+alter table collection
+    modify contract_status int null;
+
+drop index index_collection_folder_nft_nft_id on collection_folder_nft;
+
+create index index_collection_folder_nft_nft_id
+    on collection_folder_nft (nft_id);
