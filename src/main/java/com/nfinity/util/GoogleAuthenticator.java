e1a464f6726b35d8c9e42976fb5c1f8b10b85b50
a1234 <nwang@nfinitymint.com>
2022-11-03 09:57:59 +0800

    update get QR code

--- src/main/java/com/nfinity/util/GoogleAuthenticator.java
+++ src/main/java/com/nfinity/util/GoogleAuthenticator.java
@@ -71,6 +71,7 @@ public class GoogleAuthenticator {
      * @param secret the secret that was previously generated for this user
      * @return the URL for the QR code to scan
      */
+    @Deprecated
     public static String getQRBarcodeURL(String user, String host, String secret) {
         String format = "https://www.google.com/chart?chs=200x200&chld=M%%7C0&cht=qr&chl=otpauth://totp/%s@%s%%3Fsecret%%3D%s";
         return String.format(format, user, host, secret);
