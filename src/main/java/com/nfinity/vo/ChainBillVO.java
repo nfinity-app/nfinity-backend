9b9f7af27172673717d2088c745a782343c0e29f
a1234 <nwang@nfinitymint.com>
2022-11-03 18:35:10 +0800

    update wallet api

--- src/main/java/com/nfinity/vo/ChainBillVO.java
+++ src/main/java/com/nfinity/vo/ChainBillVO.java
@@ -1,5 +1,6 @@
 package com.nfinity.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,7 +15,9 @@ public class ChainBillVO {
     private String reqId;
     private int billType;
     private String address;
+    @JsonProperty("to_address")
     private String to;
+    @JsonProperty("tx_type")
     private int txType;
     private String firebolcksTxId;
     private String vaultId;
@@ -24,11 +27,14 @@ public class ChainBillVO {
     private String symbol;
     private String img;
     private String chainType;
+    @JsonProperty("tx_id")
     private String txId;
+    @JsonProperty("amount")
     private BigDecimal qty;
     private int status;
     private long blockNum;
     private long blockTime;
+    @JsonProperty("create_time")
     private Timestamp createTime;
     private String comment;
 }
